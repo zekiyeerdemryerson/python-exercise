828c2acba12075660614b747c2c6582db02635a9
Zekiye Erdem <zekiye.erdem@ryerson.ca>
2020-11-10 14:59:22 -0500

    Update Lab7.py

--- Lab7.py
+++ Lab7.py
@@ -32,11 +32,58 @@ def linearSearchFor(target,iList):
             break
             
     return position
+
+    
+def binarySearch(target,iList):
+        '''
+        This function expects sorted Lists
+        '''
+        mpi=len(iList)//2
+        pivot=iList[mpi]
+        if target==pivot:
+            return pivot,mpi
+        elif(len(iList)<=1):
+            return -1
+        elif(target<pivot):
+           iList=iList[0:len(iList)//2]
+           return binarySearch(target,iList)
+        elif(target>pivot):
+            iList=iList[len(iList)//2:len(iList)]
+            return binarySearch(target,iList)
+        else:
+            return -1
+
+def bubbleSort(iList):
+   
+    n=len(iList)
+    for i in range(n-1):
+        for x in range(0,(n-i-1)):
+            if iList[x]>iList[x+1]:
+                iList[x],iList[x+1]=iList[x+1],iList[x]
+    return iList       
+
+
+
+
     
 
-myList=[2,4,5,6,7,8,9]
-print(linearSearch(2,myList))
-print(linearSearchFor(6,myList))
+
+
+myList=[2,5,10,6,7,8,9]
+myList.sort()
+#print(linearSearch(2,myList))
+#print(linearSearchFor(2,myList))
+r=binarySearch(7,myList)
+print(r)
+s=bubbleSort(myList)
+print(s)
+
+
+#nList=myList[0:len(myList)//2]
+#print(nList)
+
+
+
 
 
 
